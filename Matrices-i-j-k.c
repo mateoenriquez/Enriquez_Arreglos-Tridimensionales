66e59eb23da06faa88d7a9a9b0145a4402a524f7
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-11-30 16:12:51 -0500

    Nro de matrices, filas y columnas

--- Matrices-i-j-k.c
+++ Matrices-i-j-k.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    
+
+    int n, m, o;
+
+    printf("Ingresar el numero de matrices a tratar: ");
+    scanf("%d", &o);
+    printf("Ingresar el numero de filas de las matrices: ");
+    scanf("%d", &n);
+    printf("Ingresar el numero de columnas de las matrices");
+    scanf("%d", &m);
+
+    int matriz[n][m][o];
+
+
     return 0;
 }
