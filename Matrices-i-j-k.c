36115da5223cafbcfb564f46005cf799c207e812
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-11-30 16:17:15 -0500

    Matrices con cero

--- Matrices-i-j-k.c
+++ Matrices-i-j-k.c
@@ -8,10 +8,40 @@ int main(){
     scanf("%d", &o);
     printf("Ingresar el numero de filas de las matrices: ");
     scanf("%d", &n);
-    printf("Ingresar el numero de columnas de las matrices");
+    printf("Ingresar el numero de columnas de las matrices: ");
     scanf("%d", &m);
 
     int matriz[n][m][o];
+
+        for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for (int k = 0; k < o; k++)
+            {
+                matriz[i][j][k]=0;
+            }
+            
+        }
+        
+    }
+
+        for (int i = 0; i < n; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < m; j++)
+        {
+            printf("\n");
+            for (int k = 0; k < o; k++)
+            {
+                printf("%d ", matriz[i][j][k]);
+            }
+            
+        }
+        
+    }
+
+    
 
 
     return 0;
